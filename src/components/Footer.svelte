8a0a3aaa4484c8827c887a0567c850577637cf76
Anthony Powell <anthony.powell@ambyint.com>
2019-04-23 21:13:05 -0400

    Minor deployment improvements

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,5 +4,8 @@
 			<strong>Quickdex</strong> by <a href="https://anthonypowell.me/">Tony Powell</a>. The source code is licensed
 			<a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
 		</p>
+		<a href="https://www.github.com/cephalization/quickdex" class="icon is-medium">
+			<i class="fab fa-github"></i>
+		</a>
 	</div>
 </footer>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import 'bulma/css/bulma.css'
 import App from './App.svelte';
+import {getPokemon} from "./getPokemon.js";
 
 const app = new App({
 	target: document.body,
 });
 
 window.app = app;
+
+getPokemon(1)
 
 export default app;
