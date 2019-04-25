5fc28a349d1df9f670c656ee12f500db77265f29
Anthony Powell <anthony.powell@ambyint.com>
2019-04-25 15:51:40 -0400

    Improve search accessibility

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -3,14 +3,26 @@
 
   let query = "";
 
-  function onEnter(event) {
-    if (event.key !== "Enter" || !query.length) return;
+  function search() {
+    if (!query.length) return;
     getPokemon(query);
+  }
+
+  function onEnter(event) {
+    if (event.key !== "Enter") return;
+    search();
   }
 </script>
 
 <section class="section">
-	<div class="control">
-		<input on:keydown={onEnter} bind:value={query} class="input" type="text" placeholder="Search by name, e.g. 'Bulbasaur'">
-	</div>
+	<div class="columns">
+    <div class="field has-addons column is-6 is-offset-3">
+       <div class="control is-expanded">
+         <input on:keydown={onEnter} bind:value={query} class="input" type="text" placeholder="Search by name, e.g. 'Bulbasaur'">
+       </div>
+       <div class="control">
+         <button disabled={!query.length} class="button is-info" type="button" on:click={search}>Search</button>
+       </div>
+     </div>
+  </div>
 </section>
